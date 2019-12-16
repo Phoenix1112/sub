f3a4b685c7235647d64edd7ae6c7c8744ed1ad66
Phoenix1112 <42475284+Phoenix1112@users.noreply.github.com>
2019-12-16 13:09:37 +0300

    Add files via upload

---
+++ poc.py
@@ -0,0 +1,34 @@
+import pydig
+import tldextract
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+
+def main():
+	with open(args.list,"r",encoding="utf-8") as f:
+		for i in f.read().lower().split("\n"):
+			if i:
+				parse = tldextract.extract(i)
+				
+				if not len(parse.subdomain) > 63:
+					with ThreadPoolExecutor(max_workers=args.thread) as executor:
+						executor.submit(resolver,i)
+				else:
+					pass
+			else:
+				pass
+
+def resolver(subdomain):
+	dns = pydig.query(subdomain,"A")
+
+	if dns:
+		print(subdomain)
+	else:
+		pass
+
+if __name__ == "__main__":
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-l","--list",required=True,metavar="",help="Subdomain List")
+	ap.add_argument("-t","--thread",required=True,metavar="",default=5,type=int,help="thread number")
+	args = ap.parse_args()
+
+	main()
